a669547e1a068756c685bb4dc65ac74fec2e54e8
MarieBouafou <marie.bouafou@epfedu.fr>
2026-06-15 12:54:04 +0200

    Fix : resolution des conflits dans app.py

--- app.py
+++ app.py
@@ -1059,15 +1059,8 @@ def create_app():
         filieres = []
         full_role = user.get("role", "")
         if full_role.startswith("RP-RM:") and ":" in full_role:
-<<<<<<< HEAD
-<<<<<<< HEAD
+
             filieres = [f.strip() for f in full_role.split(":", 1)[1].split(";") if f.strip()]
-=======
-            filieres = full_role.split(":", 1)[1].split(",")
->>>>>>> 240ed7a (Modification du fichier admin.html et création du fichier css (ajout de la fonctionnalité : selection de la formation par RP/RM))
-=======
-            filieres = full_role.split(":", 1)[1].split(";")
->>>>>>> 29e08e9 (modification admin.css pour rendre visible la fonctionnalité filière)
 
         context = {"user": user, "filieres": filieres}
 
@@ -1125,13 +1118,8 @@ def create_app():
     
     @app.put("/api/users/{id_user}/role")
     def update_user_role(id_user: int, body: RoleUpdate, session: SessionDep):
-<<<<<<< HEAD
-        if not _is_valid_role(body.role):
-=======
         role = body.role
-        valid_simple = {"Admin", "Etudiant", "RP-RM"}
-        if role not in valid_simple and not role.startswith("RP-RM"):
->>>>>>> 240ed7a (Modification du fichier admin.html et création du fichier css (ajout de la fonctionnalité : selection de la formation par RP/RM))
+        if not _is_valid_role(role):
             return JSONResponse(
                 content={"detail": f"Rôle invalide : '{role}'"},
                 status_code=422,
